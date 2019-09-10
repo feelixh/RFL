8a513c6a393bf1181ecddd0bb3664e03f9609441
Mateus Diel <mateus.diell@gmail.com>
2019-09-09 21:04:26 -0300

    banco de teste

--- docs/banco.sql
+++ docs/banco.sql
@@ -7,3 +7,18 @@ cpf varchar (20) not null,
 sala varchar(20),
 disciplina varchar(60)
 );
+
+drop table if exists rfl;
+create table rfl(
+cpf varchar(15) not null primary key,
+imagem varchar(200) not null,
+id_imagem long not null
+);
+
+insert into unijui (rg_aluno, nome, cpf, sala, disciplina) values
+(123456, 'Mateus Diel', '01589116025', 'DT201', 'Projeto Integrador 2'),
+(789654, 'Felix Hoffmann', '45643135465', 'RD96', 'Computação Gráfica'),
+(657123, 'Talles Viecilli', '78645649816', 'RF200', 'Linguagem de Programação'),
+(894564, 'Guilherme Cargenlutti', '52454454596', 'OP350', 'Introdução à Algo'),
+(311282, 'João Pedro Cargnelutti', '89765456455', 'LK985', 'Lógica de Programação');
+
